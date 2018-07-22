d2c0b2010224b66058baa676f87fcc63d6beef50
Arnol Peralta <arnulfo_pp@hotmail.com>
2018-07-22 14:22:40 -0500

    Initial Commit

---
+++ MisRecetas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MisRecetas
+//
+//  Created by Arnol Peralta on 22/07/18.
+//  Copyright © 2018 Arnol Peralta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
